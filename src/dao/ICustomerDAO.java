609cb529f8a7b50bf040162760fad6bae4350638
zkd <matheus.reichemback.stang@gmail.com>
2024-06-16 18:05:28 -0300

    feat: criado métodos CRUD na interface ICustomerDAO

--- src/dao/ICustomerDAO.java
+++ src/dao/ICustomerDAO.java
@@ -1,4 +1,11 @@
 package dao;
 
+import domain.Customer;
+
 public interface ICustomerDAO {
+
+    public Boolean create(Customer customer);
+    public Customer read(Long cpf);
+    public void update(Customer customer);
+    public Boolean delete(Long cpf);
 }
